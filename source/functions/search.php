cbfc76dced59c0019cbbddc416a4c8f62b082344
Marchand <baptistemarchand2007@gmail.com>
2026-04-02 11:36:32 +0200

    ajout de la possibilité de rechercher un étudiant seulement ce qui on été valider quand on est pas connecter et tous quand on est admin

--- source/functions/search.php
+++ source/functions/search.php
@@ -1,10 +1,21 @@
 <?php
-function searchStudent($db, $first_name)
+function searchStudent($db, $first_name = "", $last_name = "")
 {
-    $sql = "SELECT * FROM `students`
-    WHERE `first_name` like :first_name;";
+    $sql = "SELECT * FROM `student`
+    WHERE (`first_name` like :first_name OR `last_name` like :last_name) AND `status`= 'valide'";
     $stmt = $db->prepare($sql);
-    $stmt->bindValue(':first_name', $first_name.'%',PDO::PARAM_STR);
+    $stmt->bindValue(':first_name', $first_name . '%', PDO::PARAM_STR);
+    $stmt->bindValue(':last_name', $last_name . '%', PDO::PARAM_STR);
+    $stmt->execute();
+    return $stmt;
+}
+function adminSearchStudent($db, $first_name, $last_name)
+{
+    $sql = "SELECT * FROM `student`
+    WHERE `first_name` like :first_name OR `last_name` like :last_name";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':first_name', $first_name . '%', PDO::PARAM_STR);
+    $stmt->bindValue(':last_name', $last_name . '%', PDO::PARAM_STR);
     $stmt->execute();
     return $stmt;
 }
