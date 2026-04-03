3488ced4875bfb276a7f84ecc2bd7d635b478ab1
Marchand <baptistemarchand2007@gmail.com>
2026-04-03 19:45:01 +0200

    ajout de div et des icones

--- source/partials/footer.php
+++ source/partials/footer.php
@@ -1,15 +1,33 @@
 </main>
 <!-- Pied de page -->
 <footer>
-  <p>Trombinoscope</p>
-  <nav id="nav">
-    <ul>
-      <li><a href="/">Accueil</a></li>
-      <li><a href="<?= BASE_URL ?>source/pages/search.php">Trombinoscope</a></li>
-      <li><a href="<?= BASE_URL ?>source/pages/add_student.php">Ajouter</a></li>
-      </label>
-    </ul>
-  </nav>
+  <div>
+    <div>
+      <p>Trombinoscope</p>
+      <p>La Manu</p>
+      <div>
+        <i class="ri-instagram-line"></i>
+        <i class="ri-facebook-fill"></i>
+        <i class="ri-linkedin-fill"></i>
+        <i class="ri-youtube-fill"></i>
+      </div>
+    </div>
+    <div>
+      <p>Navigation</p>
+      <nav id="nav">
+        <ul>
+          <li><a href="/">Accueil</a></li>
+          <li><a href="<?= BASE_URL ?>source/pages/search.php">Trombinoscope</a></li>
+          <li><a href="<?= BASE_URL ?>source/pages/add_student.php">Ajouter</a></li>
+          </label>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <p>Mention légale</p>
+      <a href="source/pages/pdc.php">Politique de confidentialité</a>
+    </div>
+  </div>
   <p>Copyright Justine | Baptiste © 2026</p>
 </footer>
 </body>
