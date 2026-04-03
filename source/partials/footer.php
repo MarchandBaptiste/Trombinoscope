e92c010684380cb706d70fbbcf5898520f842e13
Marchand <baptistemarchand2007@gmail.com>
2026-04-04 00:25:13 +0200

    style footer avec hover sur les liens et les icons

--- source/partials/footer.php
+++ source/partials/footer.php
@@ -2,34 +2,36 @@
 <!-- Pied de page -->
 <footer>
   <div>
-    <div>
-      <p>Trombinoscope</p>
-      <p>La Manu</p>
-      <div>
-        <a href=""><i class="ri-instagram-line"></i></a>
-        <a href=""><i class="ri-facebook-fill"></i></a>
-        <a href=""><i class="ri-linkedin-fill"></i></a>
-        <a href=""><i class="ri-youtube-fill"></i></a>
+    <div class="footer-link">
+      <div class="link">
+        <h3>Trombinoscope</h3>
+        <p>La Manu</p>
+        <div class="icons">
+          <a href=""><i class="ri-instagram-line"></i></a>
+          <a href=""><i class="ri-facebook-fill"></i></a>
+          <a href=""><i class="ri-linkedin-fill"></i></a>
+          <a href=""><i class="ri-youtube-fill"></i></a>
+        </div>
+      </div>
+      <div class="link">
+        <h3>Navigation</h3>
+        <nav id="nav">
+          <ul>
+            <li><a href="/">Accueil</a></li>
+            <li><a href="<?= BASE_URL ?>source/pages/add_student.php">Ajouter</a></li>
+            <li><a href="<?= BASE_URL ?>source/pages/search.php">Recherche</a></li>
+            <li><a href="<?= BASE_URL ?>source/pages/connexion.php">Administration</a></li>
+            </label>
+          </ul>
+        </nav>
+      </div>
+      <div class="link">
+        <h3>Mention légale</h3>
+        <a href="/source/pages/pdc.php">Politique de confidentialité</a>
       </div>
     </div>
-    <div>
-      <p>Navigation</p>
-      <nav id="nav">
-        <ul>
-          <li><a href="/">Accueil</a></li>
-          <li><a href="<?= BASE_URL ?>source/pages/add_student.php">Ajouter</a></li>
-          <li><a href="<?= BASE_URL ?>source/pages/search.php">Recherche</a></li>
-          <li><a href="<?= BASE_URL ?>source/pages/connexion.php">Administration</a></li>
-          </label>
-        </ul>
-      </nav>
-    </div>
-    <div>
-      <p>Mention légale</p>
-      <a href="source/pages/pdc.php">Politique de confidentialité</a>
-    </div>
+    <p>Copyright Justine | Baptiste © 2026</p>
   </div>
-  <p>Copyright Justine | Baptiste © 2026</p>
 </footer>
 </body>
 
