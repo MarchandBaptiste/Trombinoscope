7c3dd77979abb1efd2fd53d2e20691b8d58c9969
Marchand <baptistemarchand2007@gmail.com>
2026-04-06 23:26:42 +0200

    modif validation student

--- source/functions/validateStudent.php
+++ source/functions/validateStudent.php
@@ -1,15 +1,11 @@
 <?php
 function validateStudent($db, $studentId)
 {
-    try {
-        $sql = "UPDATE `student`
+    $sql = "UPDATE `student`
                 SET `status` = 'valide'
                 WHERE `student_id` = :studentId";
-        $stmt = $db->prepare($sql);
-        $stmt->bindValue(':studentId', $studentId, PDO::PARAM_INT);
-        $stmt->execute();
-        return $stmt->rowCount();
-    } catch (PDOException $e) {
-        die('Erreur SQL : ' . $e->getMessage());
-    }
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':studentId', $studentId, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->rowCount();
 }
